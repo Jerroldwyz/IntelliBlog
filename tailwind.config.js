937618f26fd23d1adf4a7d3928831bb37aeed32d
Jerrwyz <jerroldwongyz@gmail.com>
2024-06-02 22:36:42 +1000

    color scheme + introduction section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./*.{html,js}', './src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
